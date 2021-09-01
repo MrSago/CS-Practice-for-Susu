6f8a2c55ed6f1903250124ade60943c49c508d9b
Alexander <37180898+MrSago@users.noreply.github.com>
2021-09-01 23:58:32 +0300

    Add project files.

---
+++ lab1/AppMain.cs
@@ -0,0 +1,276 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace lab1
+{
+    static class AppMain
+    {
+        static Dictionary<string, Rectangle> rectangles = new();
+
+        static void PrintRect()
+        {
+            if (rectangles.Count <= 0)
+            {
+                Console.WriteLine("\nПрямоугольников нет на плоскости!");
+                return;
+            }
+            Console.WriteLine("\nПрямоугольники и их координаты:");
+            foreach (var r in rectangles)
+            {
+                Console.WriteLine($"Key: {r.Key} | ({r.Value.X1}, {r.Value.Y1}), ({r.Value.X2}, {r.Value.Y2})");
+            }
+        }
+
+        static void AddRect()
+        {
+            Console.Write("\nВведите ключ: ");
+            string key = Console.ReadLine();
+            if (string.IsNullOrEmpty(key))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+            if (rectangles.ContainsKey(key))
+            {
+                Console.WriteLine("Такой ключ уже существует!");
+                return;
+            }
+
+            Console.WriteLine("Введите координаты: ");
+            try
+            {
+                Console.Write("x1: ");
+                float x1 = float.Parse(Console.ReadLine());
+
+                Console.Write("y1: ");
+                float y1 = float.Parse(Console.ReadLine());
+
+                Console.Write("x2: ");
+                float x2 = float.Parse(Console.ReadLine());
+
+                Console.Write("y2: ");
+                float y2 = float.Parse(Console.ReadLine());
+
+                rectangles.Add(key, new Rectangle(x1, y1, x2, y2));
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода координат!");
+            }
+        }
+
+        static void DelRect()
+        {
+            PrintRect();
+            if (rectangles.Count <= 0)
+            {
+                return;
+            }
+
+            Console.Write("\nВведите ключ для удаления: ");
+            string key = Console.ReadLine();
+            if (string.IsNullOrEmpty(key))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+            if (!rectangles.ContainsKey(key))
+            {
+                Console.WriteLine("Такой ключ не существует!");
+                return;
+            }
+
+            rectangles.Remove(key);
+        }
+
+        static void MoveRect()
+        {
+            PrintRect();
+            if (rectangles.Count <= 0)
+            {
+                return;
+            }
+
+            Console.Write("\nВведите ключ для перемещения: ");
+            string key = Console.ReadLine();
+            if (string.IsNullOrEmpty(key))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+
+            Console.WriteLine("Выберите направление стрелками");
+            Direction direction;
+            try
+            {
+                direction = Console.ReadKey(true).Key switch
+                {
+                    ConsoleKey.LeftArrow => Direction.Left,
+                    ConsoleKey.RightArrow => Direction.Right,
+                    ConsoleKey.UpArrow => Direction.Up,
+                    ConsoleKey.DownArrow => Direction.Down,
+                    _ => throw new Exception("Invalid key")
+                };
+            }
+            catch
+            {
+                Console.WriteLine("Неизвестное направление!");
+                return;
+            }
+
+            Console.Write("Введите значение: ");
+            float value;
+            try
+            {
+                value = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода значения!");
+                return;
+            }
+            rectangles[key].Move(direction, value);
+            Console.WriteLine($"Новые координаты: ({rectangles[key].X1}, {rectangles[key].Y1}), ({rectangles[key].X2}, {rectangles[key].Y2})");
+        }
+
+        static void MinRect()
+        {
+            PrintRect();
+            if (rectangles.Count <= 0)
+            {
+                return;
+            }
+
+            Console.Write("Введите ключ первого прямоугольника: ");
+            string key1 = Console.ReadLine();
+            Console.Write("Введите ключ второго прямоугольника: ");
+            string key2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(key1) || string.IsNullOrEmpty(key2))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+            if (!rectangles.ContainsKey(key1) || !rectangles.ContainsKey(key2))
+            {
+                Console.WriteLine("Прямоугольники по ключам не найдены!");
+                return;
+            }
+
+            Rectangle rectangle;
+            try
+            {
+                rectangle = Rectangle.MinRect(rectangles[key1], rectangles[key2]);
+            }
+            catch
+            {
+                Console.WriteLine("Минимальный прямоугольник не найден!");
+                return;
+            }
+
+            Console.Write("Введите ключ нового прямоугольника");
+            string new_key = Console.ReadLine();
+            if (string.IsNullOrEmpty(new_key) || rectangles.ContainsKey(new_key))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+            rectangles.Add(new_key, rectangle);
+        }
+
+        static void UnionRect()
+        {
+            PrintRect();
+            if (rectangles.Count <= 0)
+            {
+                return;
+            }
+
+            Console.Write("Введите ключ первого прямоугольника: ");
+            string key1 = Console.ReadLine();
+            Console.Write("Введите ключ второго прямоугольника: ");
+            string key2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(key1) || string.IsNullOrEmpty(key2))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+            if (!rectangles.ContainsKey(key1) || !rectangles.ContainsKey(key2))
+            {
+                Console.WriteLine("Прямоугольники по ключам не найдены!");
+                return;
+            }
+
+            Rectangle rectangle;
+            try
+            {
+                rectangle = Rectangle.UnionRect(rectangles[key1], rectangles[key2]);
+            }
+            catch
+            {
+                Console.WriteLine("Пересечение не найдено!");
+                return;
+            }
+
+            Console.Write("Введите ключ нового прямоугольника");
+            string new_key = Console.ReadLine();
+            if (string.IsNullOrEmpty(new_key) || rectangles.ContainsKey(new_key))
+            {
+                Console.WriteLine("Ошибка ввода ключа!");
+                return;
+            }
+            rectangles.Add(new_key, rectangle);
+        }
+
+        static void Main()
+        {
+            bool cont = true;
+            while (cont)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1.Добавить прямоугольник");
+                Console.WriteLine("2.Удалить прямоугольник");
+                Console.WriteLine("3.Список прямоугольников на плоскости");
+                Console.WriteLine("4.Переместить прямоугольник");
+                Console.WriteLine("5.Построение наименьшего из двух");
+                Console.WriteLine("6.Построение пересечения");
+                Console.WriteLine("0.Выход");
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        AddRect();
+                    break;
+
+                    case ConsoleKey.D2:
+                        DelRect();
+                    break;
+
+                    case ConsoleKey.D3:
+                        PrintRect();
+                    break;
+
+                    case ConsoleKey.D4:
+                        MoveRect();
+                    break;
+
+                    case ConsoleKey.D5:
+                        MinRect();
+                    break;
+
+                    case ConsoleKey.D6:
+                        UnionRect();
+                    break;
+
+                    case ConsoleKey.D0:
+                        cont = false;
+                    break;
+
+                    default:
+                        Console.WriteLine("Неизвестная опция!");
+                    break;
+                }
+            }
+        }
+    }
+}
+
---
+++ lab1/Rectangle.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+
+namespace lab1
+{
+    enum Direction
+    {
+        Left, Right, Up, Down
+    }
+
+    class Rectangle
+    {
+        private float _x1, _y1, _x2, _y2;
+
+        public float X1 { get => _x1; set => _x1 = value; }
+        public float Y1 { get => _y1; set => _y1 = value; }
+        public float X2 { get => _x2; set => _x2 = value; }
+        public float Y2 { get => _y2; set => _y2 = value; }
+
+        public Rectangle(float x1, float y1, float x2, float y2)
+        {
+            if (x1 == x2 || x1 == y2)
+            {
+                throw new Exception("It's not rectangle");
+            }
+            _x1 = Math.Min(x1, x2);
+            _y1 = Math.Min(y1, y2);
+            _x2 = Math.Max(x1, x2);
+            _y2 = Math.Max(y1, y2);
+        }
+
+        public void Move(Direction dir, float value)
+        {
+            switch (dir)
+            {
+                case Direction.Left:
+                    _x1 -= value;
+                    _x2 -= value;
+                    break;
+
+                case Direction.Right:
+                    _x1 += value;
+                    _x2 += value;
+                    break;
+
+                case Direction.Up:
+                    _y1 += value;
+                    _y2 += value;
+                    break;
+
+                case Direction.Down:
+                    _y1 -= value;
+                    _y2 -= value;
+                    break;
+
+                default:
+                    throw new Exception("Unknown direction");
+            }
+        }
+
+        static public Rectangle MinRect(Rectangle r1, Rectangle r2)
+        {
+            return new Rectangle(Math.Min(r1.X1, r2.X1), Math.Min(r1.Y1, r2.Y1), Math.Max(r1.X1, r2.X2), Math.Max(r1.Y1, r2.Y2));
+        }
+
+        static public Rectangle UnionRect(Rectangle r1, Rectangle r2)
+        {
+            if (r2.X1 < r1.X1 && r1.X1 < r2.X2 && r2.Y1 < r1.Y1 && r1.Y1 < r2.Y2)
+            {
+                return new Rectangle(r1.X1, r1.Y1, Math.Min(r1.X2, r2.X2), Math.Min(r1.Y2, r2.Y2));
+            }
+
+            if (r1.X1 < r2.X1 && r2.X1 < r1.X2 && r1.Y1 < r2.Y1 && r2.Y1 < r1.Y2)
+            {
+                return new Rectangle(r2.X1, r2.Y1, Math.Min(r1.X2, r2.X2), Math.Min(r1.Y2, r2.Y2));
+            }
+
+            if (r1.X1 < r2.X1 && r2.X1 < r1.X2 && r1.Y1 < r2.Y2 && r2.Y2 < r1.Y1)
+            {
+                return new Rectangle(r2.X1, r2.Y2, Math.Min(r1.X2, r2.X2), Math.Min(r1.Y1, r2.Y1));
+            }
+
+            if (r2.X1 < r1.X1 && r2.X1 < r1.X2 && r2.Y1 < r1.Y2 && r2.Y2 < r1.Y1)
+            {
+                return new Rectangle(r1.X1, r1.Y2, Math.Min(r1.X2, r2.X2), Math.Min(r1.Y1, r2.Y1));
+            }
+
+            return null;
+        }
+    }
+}
+
