d6cb7fda50d3aed8b9a0dbcad99d772053e2612b
Alexander <37180898+MrSago@users.noreply.github.com>
2021-09-03 10:23:14 +0300

    lab2

---
+++ lab2/AppMain.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+
+namespace lab2
+{
+    class AppMain
+    {
+        static Vector v1, v2;
+
+        static void printVecInfo()
+        {
+            Console.WriteLine($"v1({v1.X}, {v1.Y})");
+            Console.WriteLine($"v2({v2.X}, {v2.Y})");
+        }
+
+        static void printVecInfo(Vector v)
+        {
+            Console.WriteLine($"v({v.X}, {v.Y})");
+        }
+
+        static void vecSum()
+        {
+            Vector v = v1 + v2;
+            Console.Write("v1 + v2 = ");
+            printVecInfo(v);
+        }
+
+        static void vecDif()
+        {
+            Vector v = v1 - v2;
+            Console.Write("v1 - v2 = ");
+            printVecInfo(v);
+        }
+
+        static void vecAbs()
+        {
+            Console.WriteLine($"|v1| = {v1.Abs()}");
+            Console.WriteLine($"|v2| = {v2.Abs()}");
+        }
+
+        static void scalVec()
+        {
+            double a;
+            try
+            {
+                Console.WriteLine("Введите скаляр: ");
+                a = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода!");
+                return;
+            }
+            Vector new_v1 = v1 * a;
+            Vector new_v2 = v2 * a;
+            printVecInfo();
+            printVecInfo(new_v1);
+            printVecInfo(new_v2);
+        }
+
+        static void Main()
+        {
+            double xv1, yv1, xv2, yv2;
+            try
+            {
+                Console.WriteLine("Введите координаты вектора v1:");
+                Console.Write("x=");
+                xv1 = double.Parse(Console.ReadLine());
+                Console.Write("y=");
+                yv1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите координаты вектора v2:");
+                Console.Write("x=");
+                xv2 = double.Parse(Console.ReadLine());
+                Console.Write("y=");
+                yv2 = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода!");
+                return;
+            }
+
+            v1 = new(xv1, yv1);
+            v2 = new(xv2, yv2);
+
+            bool cont = true;
+            while (cont)
+            {
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1.Информация о векторах");
+                Console.WriteLine("2.Сложение v1+v2");
+                Console.WriteLine("3.Вычитение v1-v2");
+                Console.WriteLine("4.Длины векторов |v1|, |v2|");
+                Console.WriteLine("5.Умножение на скаляр v1 и v2");
+                Console.WriteLine("0.Выход");
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        printVecInfo();
+                    break;
+
+                    case ConsoleKey.D2:
+                        vecSum();
+                    break;
+
+                    case ConsoleKey.D3:
+                        vecDif();
+                    break;
+
+                    case ConsoleKey.D4:
+                        vecAbs();
+                    break;
+
+                    case ConsoleKey.D5:
+                        scalVec();
+                    break;
+
+                    case ConsoleKey.D0:
+                        cont = false;
+                    break;
+
+                    default:
+                        Console.WriteLine("Неизвестная опция!");
+                    break;
+                }
+            }
+        }
+    }
+}
+
---
+++ lab2/Vector.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace lab2
+{
+
+    class Vector
+    {
+        private double _x, _y;
+
+        public double X => _x;
+        public double Y => _y;
+
+        public Vector(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Vector(double x1, double y1, double x2, double y2)
+        {
+            _x = x2 - x1;
+            _y = y2 - y1;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1._x + v2._x, v1._y - v2._y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1._x - v2._x, v1._y - v2._y);
+        }
+
+        public static Vector operator *(Vector v1, double a)
+        {
+            return new Vector(v1._x * a, v1._y * a);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(_x * _x + _y * _y);
+        }
+    }
+}
+
