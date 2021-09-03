d4c62a66012c82f935b3a4e2cc406f4af9eb1e2c
Alexander <37180898+MrSago@users.noreply.github.com>
2021-09-03 23:06:00 +0300

    lab4

---
+++ lab4/AppMain.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Dynamic;
+using CsvHelper;
+
+namespace lab4
+{
+    class AppMain
+    {
+        static void Main()
+        {
+            using (WebScanner scanner = new())
+            {
+                string fileName = "result.csv";
+                StreamWriter streamWriter = new(fileName);
+
+                CsvWriter csvWriter = new(streamWriter, CultureInfo.InvariantCulture);
+                streamWriter.WriteLine("sep=,");
+
+                scanner.EmailsFound += (page, emails) =>
+                {
+                    Console.WriteLine($"Page:");
+                    Console.WriteLine($"        {page}");
+                    Console.WriteLine($"Emails:");
+                    foreach (string email in emails)
+                    {
+                        Console.WriteLine($"        {email}");
+                    }
+                    Console.Write('\n');
+                };
+
+                scanner.EmailsFound += (page, emails) =>
+                {
+                    dynamic record = new ExpandoObject();
+                    record.Page = page.ToString();
+
+                    string e = string.Empty;
+                    foreach (string email in emails)
+                    {
+                        e += email + '\n';
+                    }
+                    record.Emails = e;
+
+                    List<dynamic> records = new();
+                    records.Add(record);
+                    csvWriter.WriteRecords(records);
+                };
+
+                Console.WriteLine("Web Scanner started.");
+                scanner.Scan(new Uri("https://www.susu.ru/"), 10);
+                Console.WriteLine($"Done! Data write in {fileName} file.");
+
+                csvWriter.Dispose();
+                streamWriter.Dispose();
+            }
+        }
+    }
+}
+
---
+++ lab4/WebScanner.cs
@@ -0,0 +1,94 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace lab4
+{
+    class WebScanner : IDisposable
+    {
+        private readonly HashSet<Uri> _procLinks = new();
+        private readonly WebClient _webClient = new();
+        private readonly HashSet<string> _ignoreFiles = new()
+            { ".pdf", ".xml", ".doc", ".docx" };
+        private string _domain;
+        private int _pageCount;
+        private int _maxLevel;
+
+        public event Action<Uri, IEnumerable<string>> EmailsFound;
+        private void OnEmailsFound(Uri page, ref HashSet<string> emails)
+        {
+            EmailsFound?.Invoke(page, emails);
+        }
+
+        private void Process(Uri page)
+        {
+            if (_pageCount <= 0 || _procLinks.Contains(page))
+            {
+                return;
+            }
+            _procLinks.Add(page);
+
+            string html = _webClient.DownloadString(page);
+
+            HashSet<string> emails = new();
+            foreach (Match email in Regex.Matches(html, @"([-\w]+\[dot\])*[-\w]+\[at\][-\w]+(\[dot\][-\w]+)+").Cast<Match>())
+            {
+                string e = email.Value.Replace("[at]", "@").Replace("[dot]", ".");
+                emails.Add(e);
+            }
+            if (emails.Count > 0)
+            {
+                --_pageCount;
+                OnEmailsFound(page, ref emails);
+            }
+
+            var hrefs =
+                (
+                    from href in Regex.Matches(html, @"href=""https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)""").Cast<Match>()
+                    let url = href.Value.Replace("href=", "").Trim('"')
+                    let loc = url.StartsWith("/")
+                    select new
+                    {
+                        Ref = new Uri(loc ? $"{_domain}{url}" : url),
+                        IsLocal = loc || url.StartsWith(_domain)
+                    }
+                ).ToList();
+
+            Uri[] locals = 
+                (
+                    from href in hrefs
+                    where href.IsLocal
+                    select href.Ref
+                ).ToArray();
+
+            foreach (Uri href in locals)
+            {
+                string fileEx = Path.GetExtension(href.LocalPath).ToLower();
+                int level = href.AbsolutePath.Count(f => f == '/') - 1;
+                if (!_ignoreFiles.Contains(fileEx) && level <= _maxLevel)
+                {
+                    Process(href);
+                }
+            }
+        }
+
+        public void Scan(Uri startPage, int pageCount, int maxLevel = int.MaxValue)
+        {
+            _domain = $"{startPage.Scheme}://{startPage.Host}";
+            _pageCount = pageCount;
+            _maxLevel = maxLevel;
+            _procLinks.Clear();
+            Process(startPage);
+        }
+
+        public void Dispose()
+        {
+            _webClient.Dispose();
+        }
+    }
+}
+
