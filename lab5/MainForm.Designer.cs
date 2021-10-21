22c4ef867a5ac314ed44ed92acd472fda3cbcb21
Alexander <37180898+MrSago@users.noreply.github.com>
2021-10-21 16:04:06 +0300

    lab5

--- lab5/MainForm.Designer.cs
+++ lab5/MainForm.Designer.cs
@@ -29,33 +29,86 @@ namespace lab5
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bday = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.city = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // dataGridView1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 426);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = GetFriendsInfo();
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.bday,
+            this.city,
+            this.status});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(983, 506);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 90;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Имя";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Width = 200;
+            // 
+            // bday
+            // 
+            this.bday.HeaderText = "Дата Рождения";
+            this.bday.Name = "bday";
+            this.bday.ReadOnly = true;
+            this.bday.Width = 80;
+            // 
+            // city
+            // 
+            this.city.HeaderText = "Город";
+            this.city.Name = "city";
+            this.city.ReadOnly = true;
+            // 
+            // status
+            // 
+            this.status.HeaderText = "Статус";
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            this.status.Width = 450;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(1007, 530);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private string _token;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private readonly string _token;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bday;
+        private System.Windows.Forms.DataGridViewTextBoxColumn city;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
     }
 }
 
--- lab5/MainForm.cs
+++ lab5/MainForm.cs
@@ -19,14 +19,37 @@ namespace lab5
         {
             _token = token;
             InitializeComponent();
+            InitTable();
         }
 
-        public string GetFriendsInfo()
+        private JObject GetFriendsInfo()
         {
             var client = new WebClient {Encoding = Encoding.UTF8};
-            string json = client.DownloadString($"https://api.vk.com/method/friends.get?order=hints&fields=bdate&access_token={_token}&v={Program.ApiVkVer}");
-            JObject jsonObject = JObject.Parse(json);
-            return jsonObject.ToString();
+            string json = client.DownloadString($"https://api.vk.com/method/friends.get?order=hints&fields=bdate,city,status&access_token={_token}&v={Program.ApiVkVer}");
+            return JObject.Parse(json);
+        }
+
+        private void InitTable()
+        {
+            JObject friendsInfo = GetFriendsInfo();
+            int cnt = int.Parse(friendsInfo.SelectToken("response.count").ToString());
+            for (int i = 0; i < cnt; ++i)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+
+                row.Cells[0].Value = friendsInfo["response"]["items"][i]["id"]?.ToString();
+
+                row.Cells[1].Value = friendsInfo["response"]["items"][i]["first_name"]?.ToString() + ' ' +
+                                     friendsInfo["response"]["items"][i]["last_name"]?.ToString();
+
+                row.Cells[2].Value = friendsInfo["response"]["items"][i]["bdate"]?.ToString();
+
+                row.Cells[3].Value = friendsInfo["response"]["items"][i]["city"]?["title"]?.ToString();
+
+                row.Cells[4].Value = friendsInfo["response"]["items"][i]["status"]?.ToString();
+
+                dataGridView1.Rows.Add(row);
+            }
         }
     }
 }
